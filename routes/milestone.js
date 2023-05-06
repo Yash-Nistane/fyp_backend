24555b1cf9c310e8473cc035e279ece86e629093
ShrutiSolani <shrutiusolani@gmail.com>
2023-05-06 11:40:39 +0530

    modified auction

--- routes/milestone.js
+++ routes/milestone.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createNewMilestone, updateStatus, getMilestoneByID, voteMilestone } = require("../controller/milestone");
+const { createNewMilestone, updateStatus, getMilestoneByID, voteMilestone, uploadProof } = require("../controller/milestone");
 const router = express.Router();
 
 router.post('/createNewMilestone', createNewMilestone);
 router.post('/updateStatus', updateStatus);
 router.post('/getMilestone', getMilestoneByID);
 router.post('/voteMilestone', voteMilestone);
+router.post('/uploadProof', uploadProof);
 
 module.exports = router;
